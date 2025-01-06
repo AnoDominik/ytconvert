0f48e9e2550813056a546ff39170ac3781c54f64
AnoDominik <147302107+AnoDominik@users.noreply.github.com>
2025-01-06 23:15:51 +0100

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, send_file
-import subprocess
 import os
+import subprocess
 
 app = Flask(__name__)
 
@@ -9,7 +9,6 @@ def index():
     if request.method == "POST":
         url = request.form.get("url")
         file_format = request.form.get("format")
-        
         try:
             file_name = "video.mp4" if file_format == "mp4" else "audio.mp3"
             os.makedirs("downloads", exist_ok=True)
@@ -20,22 +19,24 @@ def index():
                     "--cookies", "cookies.txt",
                     "-f", "bestaudio/best" if file_format == "mp3" else "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4",
                     "-o", f"downloads/{file_name}",
-                    url
+                    url,
                 ],
                 capture_output=True,
-                text=True
+                text=True,
             )
 
             if result.returncode != 0:
                 error_message = result.stderr
-                return render_template("converter.html", error=error_message)
+                return render_template("index.html", error=error_message)
 
             return send_file(f"downloads/{file_name}", as_attachment=True)
-        
-        except Exception as e:
-            return render_template("converter.html", error=str(e))
 
-    return render_template("converter.html")
+        except Exception as e:
+            return render_template("index.html", error=str(e))
+
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render platform esetén PORT változó
+    app.run(host="0.0.0.0", port=port, debug=True)
